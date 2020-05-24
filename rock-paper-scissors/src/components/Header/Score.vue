2ea438dae47466b228a6badbb74e1b9452f9567e
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-05-24 14:18:17 -0300

    feat: adds persisted score
    - Creates a store module to hold the score
    - Use persistedstate to save the score to localStorage

--- rock-paper-scissors/src/components/Header/Score.vue
+++ rock-paper-scissors/src/components/Header/Score.vue
@@ -5,15 +5,36 @@
 				SCORE
 			</span>
 			<span class="score-label--number">
-				12
+				{{ score }}
 			</span>
 		</h3>
 	</div>
 </template>
 
 <script>
+	import { mapState, mapMutations } from 'vuex';
+
+	import { SET_SCORE } from '@/store/modules/score/mutations';
+
 	export default {
 		name: 'Score',
+		computed: {
+			...mapState('Score', {
+				score: ({ score }) => score,
+			}),
+		},
+		created() {
+			// It will be removed when score system is implemented
+			setTimeout(() => {
+				this.setScore(2);
+			}, 1000);
+		},
+		methods: {
+			// It will be removed when score system is implemented
+			...mapMutations('Score', {
+				setScore: SET_SCORE,
+			}),
+		},
 	};
 </script>
 
